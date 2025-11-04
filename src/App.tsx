41d8e077d5e71028418e2c73b6b3c5b0dbca4b5c
Aleksandr N Potashev <anipotashev@sberbank.ru>
2025-11-04 16:28:30 +0300

    замена дететка мобилы по размеру экрана на либу react-device-detect

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@ import './App.css'
 import {ThemeProvider} from "@/components/common/theme-provider.tsx";
 import {WsGate} from "@/components/common/WsGate.tsx";
 import {MainElement} from "@/components/desktop/MainElement.tsx";
-import {useIsMobile} from "@/hooks/use-mobile.ts";
 import {MainElementMobile} from "@/components/mobile/MainElementMobile.tsx";
+import { isMobile, isTablet } from "react-device-detect"
 
 function App() {
 
     const wsUrl = import.meta.env.VITE_BACKEND_URL;
     const wsUrl2 = wsUrl.startsWith("ws") ? wsUrl : `${location.protocol === "https:" ? "wss" : "ws"}://${location.host}${wsUrl}`
-    const isMobile = useIsMobile();
     return (
         <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-            <WsGate url={wsUrl2}>{isMobile ?  <MainElementMobile/> : <MainElement/> }
+            <WsGate url={wsUrl2}>{isMobile || isTablet ?  <MainElementMobile/> : <MainElement/> }
             </WsGate>
         </ThemeProvider>
     )
